8abc79220d7afd9ba3a8bbe88e39a6bb3639c17b
js-arsalan <oarsalan17@gmail.com>
2025-05-08 00:50:53 +0500

    scope

---
+++ scopes2.js
@@ -0,0 +1,50 @@
+//var c = 300
+
+
+let a = 300
+if (true){
+    let  a = 10
+const b = 20
+
+//console.log("inner:", a);
+
+
+    
+}
+
+
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+
+function one(){
+    const username = "arsalan"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    //console.log(website);
+
+   // two()
+    
+
+}
+ //one()
+
+
+ console.log( (addone (3)));
+ 
+ function addone(num){
+    return num + 4
+ }
+ 
+
+ const addTwo = function(num){
+    return num + 5
+ }
+
+ addTwo(6)
