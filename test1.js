893189e7b1babbc050c2bed6a80470fa8996e70e
js-arsalan <oarsalan17@gmail.com>
2025-06-09 02:34:39 +0500

    test

---
+++ test1.js
@@ -0,0 +1,14 @@
+console.log (arsalan)
+
+const veiw = {
+  name : "arsalan",
+  age:18,
+  email: "ali13@gmail.com",
+  isLoggedIn:false,
+  lastLogindays: ["Monday","saturday"],
+  greeting: function(){
+    console.log("hello js user");
+    
+  },
+}
+console.log(veiw.name);
